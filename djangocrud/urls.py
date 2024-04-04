46c2b33763de4d8fd0c7be0c0048cd00bedbaf99
Jaredsillo <vazquezroman.angeljared@utacapulco.edu.mx>
2024-04-04 00:20:43 -0600

    first commit

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -1,8 +1,9 @@
+
 """
 URL configuration for djangocrud project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,15 +20,20 @@ from django.urls import path
 from tasks import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
-    path('signup/', views.signup, name='signup'),
-    path('tasks/', views.tasks, name='tasks'),
-    path('tasks_completed/', views.tasks_completed, name='tasks_completed'),
-    path('logout/', views.signout, name='logout'),
-    path('signin/', views.signin, name='signin'),
-    path('create_task/', views.create_task, name='create_task'),
-    path('tasks/<int:task_id>', views.task_detail, name='task_detail'),
-    path('taks/<int:task_id>/complete', views.complete_task, name='complete_task'),
-    path('tasks/<int:task_id>/delete', views.delete_task, name='delete_task'),
+
+    path('adminLuN4/', admin.site.urls),
+    #path('',views.helloword ),
+    path('',views.home, name='home'),
+    path('signup/',views.signup, name='signup'),
+    path('task/',views.task, name='task'),
+    path('task_complete/',views.vista_tarea_Completa, name='vista_tarea_Completa'),
+    path('logout/',views.cerrarSesion, name='cerrarSesion'),
+    path('iniciaeSesion/',views.iniciaeSesion, name='iniciaeSesion'),
+    path('task/create/',views.create_task, name='create_task'),
+    
+    #Se agrega el filtro task_id para mostrar la tarea específica
+    path('task/<int:task_id>/',views.detalles, name='detalles'),
+
+    path('task/<int:task_id>/complete',views.tarea_completada, name='tarea_completada'),
+      path('task/<int:task_id>/delete',views.eliminar_tarea, name='eliminar_tarea'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,113 +1,151 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.models import User
-from django.db import IntegrityError
 from django.utils import timezone
+from django.shortcuts import render, redirect, get_object_or_404
+
+#Librería de formulario
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .form import TaskForm
+
+#Login
+from django.contrib.auth import login, logout, authenticate
+
+#Librería para protección de rutas
 from django.contrib.auth.decorators import login_required
+
+#Librería para validar usuarios
+from django.contrib.auth.models import User
+
+from django.http import HttpResponse
+
+from django.db import IntegrityError
+
 from .models import Task
-from .forms import TaskForm
 
-# Create your views here.
-
-
-def signup(request):
-    if request.method == 'GET':
-        return render(request, 'signup.html', {"form": UserCreationForm})
-    else:
-
-        if request.POST["password1"] == request.POST["password2"]:
-            try:
-                user = User.objects.create_user(
-                    request.POST["username"], password=request.POST["password1"])
-                user.save()
-                login(request, user)
-                return redirect('tasks')
-            except IntegrityError:
-                return render(request, 'signup.html', {"form": UserCreationForm, "error": "Username already exists."})
-
-        return render(request, 'signup.html', {"form": UserCreationForm, "error": "Passwords did not match."})
-
-
-@login_required
-def tasks(request):
-    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
-    return render(request, 'tasks.html', {"tasks": tasks})
-
-
-@login_required
-def tasks_completed(request):
-    tasks = Task.objects.filter(
-        user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
-    return render(request, 'tasks.html', {"tasks": tasks})
-
-
-@login_required
-def create_task(request):
-    if request.method == "GET":
-        return render(request, 'create_task.html', {"form": TaskForm})
-    else:
-        try:
-            form = TaskForm(request.POST)
-            new_task = form.save(commit=False)
-            new_task.user = request.user
-            new_task.save()
-            return redirect('tasks')
-        except ValueError:
-            return render(request, 'create_task.html', {"form": TaskForm, "error": "Error creating task."})
-
-
-def home(request):
+#Se define función home 
+def home (request):
     return render(request, 'home.html')
 
+#Se define función signup/registrar
+def signup (request):
+    if request.method == 'GET':
+       return render(request, 'signup.html', {
+        #formulario
+         'form': UserCreationForm
+     })
+
+    else:
+        #Validando las contraseñas 
+        if request.POST['password1'] == request.POST['password2']:
+           #Cachar posibles errores en la bd con Try y except
+            try: 
+            #registrando usuario
+                user= User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+            #Guardando usuario en la BD
+                user.save()
+                login(request,user)
+                return redirect ('task')
+            except IntegrityError: 
+              return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error':'El usuario ya existe'
+                }) 
+        return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error':'Las contraseñas no coinciden'
+                }) 
+
+#Se define función task 
+@login_required
+def task (request):
+    #Task.objects.all() para ver todos las consultas
+    #Se realiza la consulta sólo del usuario actual 
+    task = Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'task.html', {
+        'tareas' : task
+    })
 
 @login_required
-def signout(request):
+def cerrarSesion (request):
     logout(request)
     return redirect('home')
 
-
-def signin(request):
+def iniciaeSesion (request):
     if request.method == 'GET':
-        return render(request, 'signin.html', {"form": AuthenticationForm})
-    else:
-        user = authenticate(
-            request, username=request.POST['username'], password=request.POST['password'])
-        if user is None:
-            return render(request, 'signin.html', {"form": AuthenticationForm, "error": "Username or password is incorrect."})
-
-        login(request, user)
-        return redirect('tasks')
-
-
-@login_required
-def task_detail(request, task_id):
+        return render (request, 'iniciaeSesion.html',{
+       'form': AuthenticationForm
+        })
+    else:   
+          user = authenticate(
+              request, username=request.POST['username'], password=request.POST ['password'])
+          if user is None: 
+           return render (request, 'iniciaeSesion.html',{
+            'form': AuthenticationForm, 
+            'error': 'Usuario o contraseña son incorrectos'
+          })
+          else: 
+             #guarda la sesión mediante cookies
+              login(request,user)
+              return redirect('task')
+          
+@login_required              
+def create_task  (request):
     if request.method == 'GET':
-        task = get_object_or_404(Task, pk=task_id, user=request.user)
-        form = TaskForm(instance=task)
-        return render(request, 'task_detail.html', {'task': task, 'form': form})
-    else:
+        return render(request, 'create_task.html',{
+        'form':TaskForm
+    })
+    else: 
         try:
+            form = TaskForm (request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect ('task')
+        except ValueError: 
+             return render(request, 'create_task.html',{
+                 'form':TaskForm,
+                 'error': 'Introduce un dato válido'
+            })
+
+@login_required        
+def detalles (request, task_id) :
+    if request.method == 'GET':
+        #Con get_object_or_404 cachamos el error de entrar a ids que no existe
+        #Además de lo anterior, las tareas se filtrarán por su respectivo id -> pk=task_id
+        #Con user=request.user es para que el usuario sólo pueda ver sus tareas y no de otros
+        task = get_object_or_404(Task, pk=task_id, user=request.user)
+        #Se manda a traer nuevamente el formulario, con instance = task, es el objecto del id de esa tarea
+        form = TaskForm(instance=task)
+        return render(request, 'detalles.html',{'task':task, 'form': form})
+    else: 
+        try: 
             task = get_object_or_404(Task, pk=task_id, user=request.user)
+            #Se actualiza el formulario con los nuevos datos 
             form = TaskForm(request.POST, instance=task)
             form.save()
-            return redirect('tasks')
-        except ValueError:
-            return render(request, 'task_detail.html', {'task': task, 'form': form, 'error': 'Error updating task.'})
-
+            return redirect('task')
+        except ValueError: 
+            return render(request, 'detalles.html',
+            {'task':task, 'form': form, 'error': 'Error al actualizar tarea'})
 
 @login_required
-def complete_task(request, task_id):
+def tarea_completada(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
         task.datecompleted = timezone.now()
         task.save()
-        return redirect('tasks')
-
+        return redirect('task')
+    
+@login_required
+def eliminar_tarea(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':  
+        task.delete()
+        return redirect('task')
 
 @login_required
-def delete_task(request, task_id):
-    task = get_object_or_404(Task, pk=task_id, user=request.user)
-    if request.method == 'POST':
-        task.delete()
-        return redirect('tasks')
+def vista_tarea_Completa (request):
+    #datecompleted__isnull=False, muestra los datos que estén rellenados en el campo datecompleted 
+    #Se realiza la consulta sólo del usuario actual
+    task = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'task.html', {
+        'tareas' : task
+    })
